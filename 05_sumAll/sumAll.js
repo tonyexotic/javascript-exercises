3c65726053cc5929ab3e024bf64a3c55e2552c13
tonyexotic <72349017+tonyexotic@users.noreply.github.com>
2024-09-28 14:13:40 +0200

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && num1 > 0 && Number.isInteger(num2) && num2 > 0)  {
+        let index = num1;
+        let end = num2;
+        
+        if (num2 < num1) {
+            index = num2;
+            end = num1;
+        } 
+    
+        let sum = index;
 
+        while (index < end) {
+            sum += (index+1) ;
+            index ++
+        }
+        return sum ;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
