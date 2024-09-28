6e3f9b9aa0c166067ab9f7cb6ea5b91347af07b5
tonyexotic <72349017+tonyexotic@users.noreply.github.com>
2024-09-28 13:24:52 +0200

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...elements) {
+    let index = 0;
+    for (const element of elements){
+        if (array.includes(element)){ 
+            array = array.filter((filtre) => filtre !== element) 
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
