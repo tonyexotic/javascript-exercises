8c2a819112867e8b4dc80e90d3289ab72665b695
tonyexotic <72349017+tonyexotic@users.noreply.github.com>
2024-09-28 10:58:38 +0200

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    reversedArray = stringArray.reverse();
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
